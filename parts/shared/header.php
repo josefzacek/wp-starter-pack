e59517cb5eac0245cf6a9ce824b4ad78f943afa9
Josef Zacek <info@josefzacek.com>
2014-06-28 22:40:35 +0100

    added wp_nav_menu(); + placeholder for logo with links

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -2,4 +2,17 @@
 	<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
 	<?php bloginfo( 'description' ); ?>
 	<?php get_search_form(); ?>
+	
+	
+	
+   <a href="<?php echo home_url(); ?>"> 
+    	<img src="<?php bloginfo('template_url'); ?>/images/IMAGE-HERE" alt="">
+   </a>
+    
+   <nav>
+   	<?php
+       		$defaults = array('container' => false);
+		wp_nav_menu(); 
+	?>        
+    </nav>
 </header>
