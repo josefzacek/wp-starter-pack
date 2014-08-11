40f6efe8d0097cce5c16880d5d745a9474237d7b
Josef Zacek <info@josefzacek.com>
2014-08-11 13:26:29 +0100

    added custom color in WYSIWYG editor

--- functions.php
+++ functions.php
@@ -74,16 +74,16 @@
 	
 	/* custom login logo */
 	function my_login_logo() { ?>
-    <style type="text/css">
-        body.login div#login h1 a {
-        	background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/IMAGE_NAME_HERE);
-		background-size:auto;
-		width:auto;
-        	height: 97px;
-        }
-    </style>
-    <?php }
-    add_action( 'login_enqueue_scripts', 'my_login_logo' );
+	    <style type="text/css">
+	        body.login div#login h1 a {
+	        	background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/IMAGE_NAME_HERE);
+			background-size:auto;
+			width:auto;
+	        	height: 97px;
+	        }
+	    </style>
+	 <?php }
+        add_action( 'login_enqueue_scripts', 'my_login_logo' );
 
 	function my_login_logo_url() {
     	return get_bloginfo( 'url' );
@@ -97,23 +97,39 @@
 
 
 
-	/* remove <p> tag around images */
+	// remove <p> tag around images 
 	function filter_ptags_on_images($content){
    		return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
 	}
 	add_filter('the_content', 'filter_ptags_on_images');
 
-
 	// remove default wordpress gallery formating
 	add_filter( 'use_default_gallery_style', '__return_false' );
 	
-
 	// add data-lightbox="gallery" to wp gallery for use with lightbox
 	add_filter('wp_get_attachment_link', 'rc_add_rel_attribute');
 	function rc_add_rel_attribute($link) {
 		global $post;
 		return str_replace('<a href', '<a data-lightbox="gallery" href', $link);
 	}
+	
+	// add custom color to WYSIWYG editor 
+	function my_mce4_options( $init ) {
+	$default_colours = '
+	    "000000", "Black",        "993300", "Burnt orange", "333300", "Dark olive",   "003300", "Dark green",   "003366", "Dark azure",   "000080", "Navy Blue",      "333399", "Indigo",       "333333", "Very dark gray", 
+	    "800000", "Maroon",       "FF6600", "Orange",       "808000", "Olive",        "008000", "Green",        "008080", "Teal",         "0000FF", "Blue",           "666699", "Grayish blue", "808080", "Gray", 
+	    "FF0000", "Red",          "FF9900", "Amber",        "99CC00", "Yellow green", "339966", "Sea green",    "33CCCC", "Turquoise",    "3366FF", "Royal blue",     "800080", "Purple",       "999999", "Medium gray", 
+	    "FF00FF", "Magenta",      "FFCC00", "Gold",         "FFFF00", "Yellow",       "00FF00", "Lime",         "00FFFF", "Aqua",         "00CCFF", "Sky blue",       "993366", "Brown",        "C0C0C0", "Silver", 
+	    "FF99CC", "Pink",         "FFCC99", "Peach",        "FFFF99", "Light yellow", "CCFFCC", "Pale green",   "CCFFFF", "Pale cyan",    "99CCFF", "Light sky blue", "CC99FF", "Plum",         "FFFFFF", "White"
+	';
+	$custom_colours = '
+	    "CC911B", "Custom Gold", "294B8E", "Custom Blue"
+	';
+	$init['textcolor_map'] = '['.$default_colours.','.$custom_colours.']'; // build colour grid default+custom colors
+	$init['textcolor_rows'] = 6; // enable 6th row for custom colours in grid
+	return $init;
+	}
+	add_filter('tiny_mce_before_init', 'my_mce4_options');
 
 
 	/* ========================================================================================================================
