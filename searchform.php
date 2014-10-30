cfc4e8532a9e80ae2f57f28e3401c6d12a10fab2
Josef Zacek <info@josefzacek.com>
2014-10-30 16:35:12 +0000

    php link update

--- searchform.php
+++ searchform.php
@@ -4,5 +4,5 @@ use this template for custom search form
 
 <form action="<?php echo home_url( '/' ); ?>" method="get">
 		<input type="text" placeholder="Search" name="s" id="search" value="<?php the_search_query(); ?>" required/>
-		<input type="image" alt="Search" src="<?php bloginfo( 'template_url' ); ?>/images/search-button.png" />
+		<input type="image" alt="Search" src="<?php echo esc_url( get_template_directory_uri()); ?>/images/search-button.png" />
 </form>
