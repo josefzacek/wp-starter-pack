3a7239964736e86a89f1398de2cceef407191688
Josef Zacek <info@josefzacek.com>
2014-10-30 14:31:48 +0000

    added required attribute

--- searchform.php
+++ searchform.php
@@ -3,6 +3,6 @@ use this template for custom search form
 -->
 
 <form action="<?php echo home_url( '/' ); ?>" method="get">
-		<input type="text" placeholder="Search" name="s" id="search" value="<?php the_search_query(); ?>" />
+		<input type="text" placeholder="Search" name="s" id="search" value="<?php the_search_query(); ?>" required/>
 		<input type="image" alt="Search" src="<?php bloginfo( 'template_url' ); ?>/images/search-button.png" />
 </form>
