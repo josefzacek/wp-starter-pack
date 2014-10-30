414dd067a26ecae7464410941718cf6dd1d573a7
Josef Zacek <info@josefzacek.com>
2014-10-30 15:54:34 +0000

    added link for image

--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -1,4 +1,6 @@
 	
 	<footer>
 		&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.
+		
+		<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/IMAGE_HERE" alt="Chartered Tax Adviser" class="footer-aiti">
 	</footer>
