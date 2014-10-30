f0e91b42551932909f2993952988085b1ea3c2a5
Josef Zacek <info@josefzacek.com>
2014-10-30 14:20:46 +0000

    custom search form
    adding this file will tell WordPress to use code in here for search form instead of WordPress default one

---
+++ searchform.php
@@ -0,0 +1,8 @@
+<!--
+use this template for custom search form
+-->
+
+<form action="<?php echo home_url( '/' ); ?>" method="get">
+		<input type="text" placeholder="Search" name="s" id="search" value="<?php the_search_query(); ?>" />
+		<input type="image" alt="Search" src="<?php bloginfo( 'template_url' ); ?>/images/search-button.png" />
+</form>
