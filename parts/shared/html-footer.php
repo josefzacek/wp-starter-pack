c20d04473d05e0ea5edee1403b031cdfb2a6e183
Josef Zacek <info@josefzacek.com>
2014-10-30 15:45:54 +0000

    php code change
    bloginfo('template_url')          changed to        echo esc_url( get_template_directory_uri() )

--- parts/shared/html-footer.php
+++ parts/shared/html-footer.php
@@ -1,7 +1,7 @@
 
 	<?php wp_footer(); ?>
 	
-	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/scripts.js"></script>
+	<script type="text/javascript" src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/scripts.js"></script>
 	
 	</body>
 </html>
