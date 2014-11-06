1bcc2faa84483c0401a875148368de08616b1ffb
Josef Zacek <info@josefzacek.com>
2014-11-06 16:42:08 +0000

    page title formatting added

--- functions.php
+++ functions.php
@@ -151,6 +151,28 @@
 		/* Page attributes meta box. */
 		//remove_meta_box( 'pageparentdiv', 'page', 'side' );
 	}
+	/* ================================= page title formating ================================= */
+	function custom_wp_title( $title, $sep ) {
+		global $paged, $page;
+
+		if ( is_feed() )
+			return $title;
+
+		// Add the site name.
+		$title .= get_bloginfo( 'name' );
+
+		// Add the site description for the home/front page.
+		$site_description = get_bloginfo( 'description', 'display' );
+		if ( $site_description && ( is_home() || is_front_page() ) )
+			$title = "$title $sep $site_description";
+
+		// Add a page number if necessary.
+		if ( $paged >= 2 || $page >= 2 )
+			$title = "$title $sep " . sprintf( __( 'Page %s', 'alanmoore' ), max( $paged, $page ) );
+
+		return $title;
+	}
+	add_filter( 'wp_title', 'custom_wp_title', 10, 2 );
 
 
 
