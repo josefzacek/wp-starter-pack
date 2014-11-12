32e21bd981e6c03bb12d2a144722136211e1e7c5
Josef Zacek <info@josefzacek.com>
2014-11-12 11:48:56 +0000

    fit slideshow on load
    added script to fit slideshow on load

--- parts/shared/html-footer.php
+++ parts/shared/html-footer.php
@@ -3,5 +3,11 @@
 	
 	<script type="text/javascript" src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/scripts.js"></script>
 	
+	
+	<!--	fits slideshow on load	-->
+	<script>  
+   		jQuery(window).bind("load", function(){ jQuery(window).resize(); });  
+	</script>
+	
 	</body>
 </html>
