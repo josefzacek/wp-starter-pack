8b6cea75afc452b0ac8f3d9ef8a5bf278414c4c0
Josef Zacek <joezacek@gmail.com>
2015-01-07 16:56:57 +0000

    search only posts not pages

--- searchform.php
+++ searchform.php
@@ -1,8 +1,10 @@
 <!--
 use this template for custom search form
+last input tells wp to search in posts only
 -->
 
 <form action="<?php echo home_url( '/' ); ?>" method="get">
 		<input type="text" placeholder="Search" name="s" id="search" value="<?php the_search_query(); ?>" required/>
 		<input type="image" alt="Search" src="<?php echo esc_url( get_template_directory_uri()); ?>/images/search-button.png" />
+		<input type="hidden" value="post" name="post_type" id="post_type" />
 </form>
